6d9a1793ea763168f26047c6e4c210ff4dc4696f
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-02-06 07:52:52 -0400

    maquetado (header)

--- js/script.js
+++ js/script.js
@@ -1,2 +1,48 @@
 
     M.AutoInit();
+
+   
+    class SiemaWithDots extends Siema {
+
+        addDots() {
+          this.dots = document.createElement('div');
+          this.dots.classList.add('dots');
+  
+          for(let i = 0; i < this.innerElements.length; i++) {
+            const dot = document.createElement('button');
+  
+            dot.classList.add('dots__item');
+  
+            dot.addEventListener('click', () => {
+              this.goTo(i);
+            })
+  
+            this.dots.appendChild(dot);
+          }
+  
+          this.selector.parentNode.insertBefore(this.dots, this.selector.nextSibling);
+        }
+  
+        updateDots() {
+          for(let i = 0; i < this.dots.querySelectorAll('button').length; i++) {
+            const addOrRemove = this.currentSlide === i ? 'add' : 'remove';
+            this.dots.querySelectorAll('button')[i].classList[addOrRemove]('dots__item--active');
+          }
+        }
+      }
+  
+      const siema = new SiemaWithDots({
+        onInit: function(){
+          this.addDots();
+          this.updateDots();
+        },
+  
+        onChange: function(){
+          this.updateDots()
+        },
+        selector: '.siema',
+        duration: 200,
+        easing: 'ease-out',
+        loop: true
+      });
+  
