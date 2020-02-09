e25778a5897db331c73ffb1b2ea18abd8826c253
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-02-09 19:58:24 -0400

    algunos mejoras y configurando correo

--- sendmail.php
+++ sendmail.php
@@ -3,28 +3,32 @@
     use PHPMailer\PHPMailer\PHPMailer;
     require 'vendor/autoload.php';
 
+    $email = $_POST['email'];
+    $name  = $_POST['name'];
+    $message = $_POST['message'];
+    $phone = $_POST['phone'];
+    $country = $_POST['country'];
+
     $mail = new PHPMailer;
 
     $mail->isSMTP();
     $mail->SMTPDebug = 2;
-    
-    $mail->Host = 'mx1.hostinger.com';
+
+    $mail->Host = 'smtp.mailtrap.io';
     $mail->Port = 587;
     $mail->SMTPAuth = true;
-    $mail->Username = 'test@hostinger-tutorials.com';
-    $mail->Password = 'EMAIL_ACCOUNT_PASSWORD';
+    $mail->Username = '6f515f07b3be9c';
+    $mail->Password = '1dea3b29c155a6';
 
-    $mail->setFrom('test@hostinger-tutorials.com', 'Your Name');
-    $mail->addReplyTo('reply-box@hostinger-tutorials.com', 'Your Name');
-    $mail->addAddress('example@gmail.com', 'Receiver Name');
+    $mail->setFrom('info@woney.com', 'Información Woney');
+    $mail->addAddress($email, $name);
 
-    $mail->Subject = 'PHPMailer SMTP message';
+    $mail->Subject = 'Información desde Woney';
 
-    $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+    // $mail->msgHTML(file_get_contents('message.html'), __DIR__);
 
-    $mail->AltBody = 'This is a plain text message body';
+    $mail->AltBody = $message;
 
-    $mail->addAttachment('test.txt');
 
     if (!$mail->send()) {
         echo 'Mailer Error: ' . $mail->ErrorInfo;
