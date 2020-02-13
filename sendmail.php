8852257d500e245b1975b8cf829c2fc656e9dbb4
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2020-02-13 10:00:54 -0400

    correos

--- sendmail.php
+++ sendmail.php
@@ -33,11 +33,8 @@
                 $message
         ";
 
-    echo $body;
-
     $mail->AltBody = $body;
     $mail->Body = $body;
-
 
     if (!$mail->send()) {
         echo 'Mailer Error: ' . $mail->ErrorInfo;
